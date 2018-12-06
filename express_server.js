8928b361bada8dfb7a3b5a9e51c67675ba3982e3
DanielKwanUkYoo <kwanukyoo0722@gmail.com>
2018-12-06 04:19:30 +0000

    added signin/logout/cookies

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const PORT = 8080;
 const app = express();
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 //route?
 var urlDatabase = {
@@ -14,50 +16,68 @@ var urlDatabase = {
 
 //url pg
 app.get("/urls", (request, response) => {
-  let templateVars = { urls: urlDatabase };
+  const username = request.cookies["username"];
+  let templateVars = { urls: urlDatabase, username: username };
   response.render("urls_index", templateVars);
 });
 
 //client input pg
 app.get("/urls/new", (request, response) => {
-  response.render("urls_new");
+  const username = request.cookies["username"];
+  let templateVars = { username: username };
+  response.render("urls_new", templateVars);
 });
 
 //after input generate random string
 app.post("/urls/new", (request, response) => {
-  let shortRandomURL = randomString();
-
+  const shortRandomURL = randomString();
   urlDatabase[shortRandomURL] = request.body.longURL;
   response.redirect("/urls");
+
 })
+
+//login/setting cookie
+app.post("/urls/login", (request, response) => {
+  response.cookie("username", request.body.username);
+  response.redirect("/urls");
+})
+
+//logout clearcookies
+app.post("/urls/logout", (request, response) => {
+  response.clearCookie("username");
+  response.redirect("/urls");
+})
+
 
 //Delete
 app.post("/urls/:id/delete", (request,response) => {
   delete urlDatabase[request.params.id];
-  response.redirect("/urls")
+  response.redirect("/urls");
 })
 
 //Edit/Add
 app.post("/urls/:id", (request,response) => {
   urlDatabase[request.params.id] = request.body.newURL;
-  response.redirect("/urls")
+  response.redirect("/urls");
 })
 
 app.get("/u/:shortURL", (request, response) => {
-  let longURL = urlDatabase[request.params.shortURL];
+  const longURL = urlDatabase[request.params.shortURL];
   response.redirect(longURL);
 })
 
 //browser dir
 app.get("/urls/:id", (request, response) => {
+  const username = request.cookies["username"];
   let templateVars = { shortURL: request.params.id,
-                       dataKey: urlDatabase
+                       dataKey: urlDatabase,
+                       username: username
                      };
-  response.render("urls_show", templateVars)
+  response.render("urls_show", templateVars);
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app is listening on port ${PORT}`)
+  console.log(`Example app is listening on port ${PORT}`);
 });
 
 
